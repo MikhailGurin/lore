8b9663900dcabbc20364e182a8f2f2704460f9db
Mikhail Gurin <mikhail.gurin@gmail.com>
2022-05-22 20:18:26 +0300

    Несколько определений

---
+++ articles/navigation.md
@@ -0,0 +1,9 @@
+
+###Общее
+[HTTP](HTTP.md)
+
+###Архитектура
+[OOP](OOP.md)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Models](models.md)
+
+### БД
+[Индексы](indexes.md)
---
+++ hints/kafka/definitions.py
@@ -0,0 +1,9 @@
+
+"""
+Confluent Client
+https://github.com/confluentinc/confluent-kafka-python
+
+group.id
+Группа это набор consumer`ов одного приложения, по сути несколько потоков обработки под одним id.
+В самом простом случае в группе один consumer и соответственно в один поток читает.
+"""
